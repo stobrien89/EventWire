a2158ce7bfef1d3986afc40f91a7ace9e7a8a841
Chozen <chozen2see@augusts-mbp.home>
2020-05-31 22:50:35 -0400

    created event model

--- models/event.js
+++ models/event.js
@@ -1,10 +1,32 @@
 const mongoose = require('../db/connection')
 
-const EventSchema = new mongoose.Schema({
+const eventSchema = new mongoose.Schema({
   name: String,
-  type: String
+  type: String,
+  description: String,
+  price_per_person: String,
+  rating: Number,
+  tag: [String],
+  image: { data: Buffer, contentType: String },
+  address: {
+    street: String,
+    unit: String,
+    city: String,
+    state: String,
+    zip: String,
+    country: String
+  },
+  contact: {
+    name: String,
+    phone: String,
+    email: String
+  },
+  destination: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Destination',
+  }
 })
 
-const Event = mongoose.model('Event', EventSchema)
+const Event = mongoose.model('Event', eventSchema)
 
 module.exports = Event
