c4e45e11e15abd6a7fac7a520afba7cd9cf05415
Chozen <chozen2see@augusts-mbp.home>
2020-06-06 02:20:12 -0400

    added conditional for contact info

--- public/js/components/Event.js
+++ public/js/components/Event.js
@@ -36,59 +36,67 @@ class Event extends React.Component {
 
       <div className="container-fluid container-height">
         <div className="container">
-          <div className="d-flex flex-column">
-            {/* <div className="d-flex flex-row event_header">
-              <div className="p-2 flex-fill"><h4 className='text-muted'>{event.destination_name} >> {event.name}</h4></div>
-
-            </div> */}
-            <div className="d-flex flex-row flex-wrap">
-              <div className="flex-column flex-fill flex-shrink-1">
-                <div className="p-2">
-                  <h4 className='text-muted'>{event.destination_name} >> {event.name}</h4>
-                  <div className="mb-3 details_text">RATING: *****</div>
-                  <div className="details_text details_label">WHEN:</div>
-                  <div className="details_text">{event.start_date} - {event.end_date}</div>
-                </div>
-
-                {this.state.address &&
-                  <div className="p-2">
-                    <div className="details_text details_label">LOCATION:</div>
-                    <div className="details_text">{`${event.address.street}`}</div>
-                    <div className="details_text">
-                      {`${event.address.city}, ${event.address.state} ${event.address.zip} ${event.address.country}`}
-                    </div>
-                    <div className="details_text text-muted"><a href={`${event.google_map}`} target="_blank">MAP</a></div>
-                  </div>
-                }
-
-                <div className="p-2 details_description">{event.description}</div>
-
-                <div className="p-2 text-muted"><span className="details_label">PRICE PER PERSON</span> ${event.price_per_person}</div>
-
+          <div className="d-flex flex-row flex-wrap flex-fill justify-content-around">
+            <div className="flex-column flex-fill flex-shrink-1 mb-4">
+              <div className="p-2">
+                <h4 className='text-muted'>
+                  <Link to={`/destination_details?d=${event.destination}`}>{event.destination_name}</Link>
+                  {' '} >> {event.name}</h4>
+                <div className="mb-3 details_text">RATING: *****</div>
+                <div className="details_text details_label">WHEN:</div>
+                <div className="details_text">{event.start_date} - {event.end_date}</div>
               </div>
-              <div className="d-flex flex-column flex-grow-1 ml-auto">
-                <div className="flex-fill"><img className="details_image" src={event.image_url} alt={event.name} /></div>
 
-                {this.state.contact &&
-                  <div className="p-3 flex-fill">
-                    <div className="mb-1 details_text details_label">CONTACT INFO:</div>
+              {this.state.address &&
+                <div className="p-2">
+                  <div className="details_text details_label">LOCATION:</div>
+                  <div className="details_text">{`${event.address.street}`}</div>
+                  <div className="details_text">
+                    {`${event.address.city}, ${event.address.state} ${event.address.zip} ${event.address.country}`}
+                  </div>
+                  <div className="details_text text-muted"><a href={`${event.google_map}`} target="_blank">MAP</a></div>
+                </div>
+              }
+
+              <div className="p-2 details_description">{event.description}</div>
+
+              <div className="d-flex flex-row event_header">
+                <div className="p-2 "><span className="details_label">PRICE PER PERSON</span> ${event.price_per_person}</div>
+                <button className="btn-secondary ml-auto">ADD TO ITINERARY</button>
+              </div>
+            </div>
+
+            <div className="d-flex flex-column flex-grow-1 ml-auto">
+              <div className="flex-fill"><img className="details_image" src={event.image_url} alt={event.name} /></div>
+
+              {this.state.contact &&
+                <div className="p-3 flex-fill justify-content-around">
+                  <div className="mb-1 details_text details_label">CONTACT INFO:</div>
+
+                  {event.contact.name &&
                     <div className="details_text">
                       <i class='far fa-address-card fa-font-size'></i>{' '}
                       {event.contact.name}
                     </div>
+                  }
+
+                  {event.contact.phone &&
                     <div className="details_text">
                       <i class="fa fa-phone-square fa-font-size"></i>{' '}
                       {event.contact.phone}
                     </div>
+                  }
+
+                  {event.contact.email &&
                     <div className="details_text">
                       <i class="fas fa-at fa-font-size"></i>{' '}
                       {event.contact.email}</div>
-                  </div>
-                }
-              </div>
+                  }
+
+                </div>
+              }
             </div>
           </div>
-
         </div>
       </div >
     )
