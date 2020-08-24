ffc512746fed0db6ea6166f04e4f0386ea25120b
Chozen <chozen2see@augusts-mbp.home>
2020-08-23 21:11:09 -0400

    final fix-event-details

--- public/js/components/Destination.js
+++ public/js/components/Destination.js
@@ -48,15 +48,10 @@ class Destination extends React.Component {
     return (
       <div className="container-fluid container-height">
         <div className="container">
-          {/* <div className="d-flex flex-row">
-            <h4 className='text-muted'>
-              {destination.name}
-            </h4>
-            <button className="btn-secondary ml-auto">FIND EVENTS</button>
-          </div> */}
-          <div className="d-flex flex-row flex-wrap flex-fill justify-content-around">
+          {/* <div className="d-flex flex-row flex-wrap flex-fill justify-content-around"> */}
+          <div className="row">
 
-            <div className="flex-column flex-fill flex-shrink-1 mb-4">
+            <div className="col-12 col-md-8 mb-4 mt-4">
 
               <div className="d-flex flex-row mb-4">
                 <h4 className='text-muted'>
@@ -65,19 +60,20 @@ class Destination extends React.Component {
                 </h4>
               </div>
 
-              <div className="flex-fill mb-3">
+              <div className="col-12 col-md-9 mb-3">
                 <img className="details_image" src={destination.image_url} alt={destination.name} />
+
               </div>
 
-              <div className="details_description mb-3">{destination.description}</div>
+              <div className="col-12 col-md-9 col-sm-9 details_description mb-3">{destination.description}</div>
 
-              <button className="btn-secondary ml-auto" onClick={this.handleClick}>FIND EVENTS</button>
+              <button className="col-12 col-md-9 col-sm-9 ml-1 btn-secondary" onClick={this.handleClick}>FIND EVENTS</button>
 
             </div>
 
-            <div className="d-flex flex-column flex-grow-1 ml-auto">
+            <div className="col-12 col-md-4 mt-4">
               {/* <h2>EXPEDIA</h2> */}
-              <div id="searchWidget" className="expedia"><iframe id="widgetIframe" src={`https://www.expedia.com/marketing/widgets/searchform/widget?wtt=1&tp1=7907203&tp2=10479934&lob=H,FH,F,CA&des=${destination.name}&wbi=6&olc=421B43&whf=7&hfc=F2AE1D&wif=4&ifc=&wbc=DC4626&wbf=4&bfc=FFFFFF&wws=2&sfs=H600FW300F&langid=1033`} width="100%" height="100%" scrolling="no" frameBorder="0"></iframe></div>
+              <div id="searchWidget" className="expedia mx-auto"><iframe id="widgetIframe" src={`https://www.expedia.com/marketing/widgets/searchform/widget?wtt=1&tp1=7907203&tp2=10479934&lob=H,FH,F,CA&des=${destination.name}&wbi=6&olc=421B43&whf=7&hfc=F2AE1D&wif=4&ifc=&wbc=DC4626&wbf=4&bfc=FFFFFF&wws=2&sfs=H600FW300F&langid=1033`} width="100%" height="100%" scrolling="no" frameBorder="0"></iframe></div>
             </div>
 
           </div>
